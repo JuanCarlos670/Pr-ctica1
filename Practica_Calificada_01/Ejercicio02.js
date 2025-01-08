be69620efefe05030fd0b6be2ff6bbb4925af2c9
jhonatanbarjag <jhonatanbarja.g@hotmail.com>
2025-01-08 18:04:35 -0500

    Resolucion ejercicio

--- Practica_Calificada_01/Ejercicio02.js
+++ Practica_Calificada_01/Ejercicio02.js
@@ -1,5 +1,5 @@
 let edad= parseInt(prompt("ingrese su edad"));
-let ingreso= parseInt(prompt("ingrese su ingreso mensual"));
+let ingreso= parseFloat(prompt("ingrese su ingreso mensual"));
 
 if (edad>=18 && ingreso>=500 && ingreso <= 2000){
     console.log("el usuario es legible para el subsidio")
